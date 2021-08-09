7f7cbfba29b817b8a7a9102e62f064a9b752a0ad
angel9015 <jc11091005@gmail.com>
2021-08-09 16:18:33 +0300

    Changes for restistor

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,30 +1,28 @@
-interface IHash {
-    [key: string]: number;
+enum ResistorValues {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9
 }
 
-const ResistorValues: IHash = {
-  "black": 0,
-  "brown": 1,
-  "red": 2,
-  "orange": 3,
-  "yellow": 4,
-  "green": 5,
-  "blue": 6,
-  "violet": 7,
-  "grey": 8,
-  "white": 9
-}
+declare type ResistorValuesType = keyof typeof ResistorValues;
 
 export class ResistorColor {
-  private colors: string[]
-
-  constructor(colors: string[]) {
-    this.colors = colors.slice(0, 2)
-    if(this.colors.length < 2) {
+  constructor(private colors: string[]) {
+    if(colors.length < 2) {
       throw new Error("At least two colors need to be present")
     }
+    this.colors = colors.slice(0, 2)
   }
-  value = (): number => {
-    return this.colors.map(v => ResistorValues[v]).reduce((a, b) => a*10 + b)
-  }
+  value = () => this.colors.map(
+    v => ResistorValues[v as ResistorValuesType].valueOf()
+  ).reduce(
+    (a, b) => a*10 + b
+  ) 
 }
