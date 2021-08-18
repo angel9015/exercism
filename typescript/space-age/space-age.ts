e7359eb0cb463c6f0e0b112635f6c2c153c78877
angel9015 <jc11091005@gmail.com>
2021-08-18 10:19:45 +0300

    Add space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,51 +1,52 @@
-const EARTH_SECONDS_IN_YEAR = 31557600;
-const MERCURY_SECONDS_IN_YEAR = 0.2408467 * EARTH_SECONDS_IN_YEAR;
-const VENUS_SECONDS_IN_YEAR = 0.61519726 * EARTH_SECONDS_IN_YEAR;
-const MARS_SECONDS_IN_YEAR = 1.8808158 * EARTH_SECONDS_IN_YEAR;
-const JUPITER_SECONDS_IN_YEAR = 11.862615 * EARTH_SECONDS_IN_YEAR;
-const SATURN_SECONDS_IN_YEAR = 29.447498 * EARTH_SECONDS_IN_YEAR;
-const URANUS_SECONDS_IN_YEAR = 84.016846 * EARTH_SECONDS_IN_YEAR;
-const NEPUTE_SECONDS_IN_YEAR = 164.79132 * EARTH_SECONDS_IN_YEAR;
+
+enum SecondsInYear {
+    Earth = 31557600,
+    Mercury = 0.2408467 * Earth,
+    Venus = 0.61519726 * Earth,
+    Mars = 1.8808158 * Earth,
+    Jupiter = 11.862615 * Earth,
+    Saturn = 29.447498 * Earth,
+    Uranus = 84.016846 * Earth,
+    Neptune = 164.79132 * Earth,
+}
+
 
 const r = (n: number) => Math.round(n*100) / 100;
 
 export default class SpaceAge {
-    seconds: number;
-
-    constructor(seconds: number) {
-        this.seconds = seconds;
-    }
+    
+    constructor(public seconds: number) {}
 
     onEarth(): number {
-        return r(this.seconds / EARTH_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Earth);
     }
 
     onMercury(): number {
-        return r(this.seconds / MERCURY_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Mercury);
     }
 
     onVenus(): number {
-        return r(this.seconds / VENUS_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Venus);
     }
 
     onMars(): number {
-        return r(this.seconds / MARS_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Mars);
     }
 
     onJupiter(): number {
-        return r(this.seconds / JUPITER_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Jupiter);
     }
 
     onSaturn(): number {
-        return r(this.seconds / SATURN_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Saturn);
     }
 
     onUranus(): number {
-        return r(this.seconds / URANUS_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Uranus);
     }
 
     onNeptune(): number {
-        return r(this.seconds / NEPUTE_SECONDS_IN_YEAR);
+        return r(this.seconds / SecondsInYear.Neptune);
     }
 
 
