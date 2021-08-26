c123450d81138efce9839f683d8e3b31056f98fa
angel9015 <jc11091005@gmail.com>
2021-08-26 14:05:36 +0300

    Small chjaneg

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,29 +1,39 @@
+enum Answer {
+  EMPTY = 'Fine. Be that way!',
+  CAPS_QUESTION = 'Calm down, I know what I\'m doing!',
+  QUESTION = 'Sure.',
+  ALL_CAPS = 'Whoa, chill out!',
+  OTHER = 'Whatever.'
+}
+
 
 const isEmpty = (input: string) => input.length == 0
 const isQuestion = (input: string) => input.endsWith('?')
 const isAllCaps = (input: string) => input.toUpperCase() === input && input.toUpperCase() !== input.toLowerCase()
+const isCapsQuestion = (input: string) => isAllCaps(input) && isQuestion(input)
+
+const Mapper: Record<keyof typeof Answer, (input: string)=>boolean> = {
+  EMPTY: isEmpty,
+  CAPS_QUESTION: isCapsQuestion,
+  QUESTION: isQuestion,
+  ALL_CAPS: isAllCaps,
+  OTHER: () => true
+}
 
 class Bob {
 
-  hey(input: string) {
+  hey(input: string): Answer {
     input = input.trim()
-    if (isEmpty(input)) {
-      return 'Fine. Be that way!'
-    }
 
-    if (isQuestion(input) && isAllCaps(input)) {
-      return 'Calm down, I know what I\'m doing!'
+    for(const key of  typeof Answer) {
+      
+        let f = Mapper[key]
+        if(f(input)) {
+          return Answer[key]
+        }
+      
     }
-
-    if (isQuestion(input)) {
-      return 'Sure.'
-    }
-    if (isAllCaps(input)) {
-      return 'Whoa, chill out!'
-    }
-    
-
-    return 'Whatever.';
+    return Answer.OTHER
   }
 }
 
